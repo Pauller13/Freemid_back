d75e35e85fb27bdf1b6b5603051ddaef249dbe87
ndrieric <patcoeric@gmail.com>
2024-10-11 11:22:10 +0000

    crud offer and skill

--- src/offer/views/offer_viewset.py
+++ src/offer/views/offer_viewset.py
@@ -1,8 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import mixins,generics
 from offer.models.offer_model import  OfferModel
 from offer.serializers.offer_serializer import OfferSerializer
 
 
-class OfferViewSet(viewsets.ModelViewSet):
+class OfferViewSet(mixins.ListModelMixin):
     queryset = OfferModel.objects.all()
     serializer_class = OfferSerializer
+
+    def get(self,request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class ClientOffreListView(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = OfferSerializer
+
+
+    def get_queryset(self):
+        client = self.request.user.client
+        return OfferModel.objects.filter(client=client)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
--- src/offer/views/offerskill_viewset.py
+++ src/offer/views/offerskill_viewset.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, generics
 from offer.models.offerskill_model import OfferSkillModel
 from offer.serializers.offerskill_serializer import OfferSkillSerializer
 
 
-class OfferSkillViewSet(viewsets.ModelViewSet):
-    queryset = OfferSkillModel.objects.all()
-    serializer_class = OfferSkillSerializer
+
+class OfferSkillViewSet(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class =OfferSkillModel
+    def get_queryset(self):
+        skills = self.request.query_params.getlist('skills')
+        return OfferSkillModel.objects.filter(skills=skills).distinct()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
